8b7f223a7b383539b663dc737d5f3f5e4acfad17
scmsal <31stephcm@gmail.com>
2025-04-21 15:20:59 -0400

    Deleted testQuery and put in state for VeggiesList.jsx
    debugging.

--- hacker-project/src/components/VeggiesList.jsx
+++ hacker-project/src/components/VeggiesList.jsx
@@ -1,13 +1,24 @@
 import { Container, ListGroup, ListGroupItem } from "react-bootstrap";
 import { veggieObjects } from "../storage.js";
 import "../App.css";
+import { useState } from "react";
 
 const VeggiesList = () => {
+  const [selectedVeggie, setSelectedVeggie] = useState("");
+
   return (
     <Container className="mx-3 w-25">
       <ListGroup>
         {veggieObjects.map((veggie) => (
-          <ListGroupItem key={veggie.id} action className="custom-hover">
+          <ListGroupItem
+            key={veggie.id}
+            action
+            className="custom-hover"
+            variant={
+              selectedVeggie === veggie.name ? "success" : "outline-success"
+            }
+            onClick={() => setSelectedVeggie(veggie.name)}
+          >
             {veggie.name}
           </ListGroupItem>
         ))}
