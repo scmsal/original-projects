60a2c0133c9e5d2205ae792dc275580f6abf6e86
scmsal <31stephcm@gmail.com>
2025-04-24 10:36:25 -0400

    added code to PlantFetcher
    add card to display details of selected plant instead of the whole list of plant data.

--- hacker-project/src/components/PlantFetcher.jsx
+++ hacker-project/src/components/PlantFetcher.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPlants } from "../features/plantsSlice";
+import { Card } from "react-bootstrap";
 
 function PlantFetcher() {
   const dispatch = useDispatch();
   // const status = useSelector((state) => state.plants.status);
   const plantNames = useSelector((state) => state.plants.plantNames);
   const plantData = useSelector((state) => state.plants.plantData);
+
+  const selectedPlant = useSelector((state) => state.plants.selectedPlant);
 
   useEffect(() => {
     if (plantNames.length > 0) {
@@ -15,10 +18,17 @@ function PlantFetcher() {
   }, [dispatch, plantNames]);
 
   return (
-    <div>
-      <h1>Plant Data</h1>
-      <pre>{JSON.stringify(plantData, null, 2)}</pre>
-    </div>
+    // <div>
+    //   <h1>Plant Data</h1>
+    //   <pre>{JSON.stringify(plantData, null, 2)}</pre>
+    // </div>
+    <Card>
+      <Card.Img></Card.Img>
+      <Card.Body>
+        <Card.Title></Card.Title>
+        <h3></h3>
+      </Card.Body>
+    </Card>
   );
 }
 
