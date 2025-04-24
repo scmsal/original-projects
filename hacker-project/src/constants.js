65f89a991fd3617d4f7566abba4aa28bc47ed6e7
scmsal <31stephcm@gmail.com>
2025-04-24 10:37:22 -0400

    created constants file
    I moved some info from storage.js because it was messing up the rendering of the VeggiesList component.

---
+++ hacker-project/src/constants.js
@@ -0,0 +1,27 @@
+const plantNamesList = [
+  "basil",
+  "bean",
+  "bell pepper",
+  "broccoli",
+  "carrot",
+  "chard",
+  "dill",
+  "kale",
+  "lettuce",
+  "onion",
+  "pea",
+  "potato",
+  "sage",
+  "thyme",
+  "tomato",
+  "zucchini",
+];
+
+const API_KEY = "0000"; //import.meta.env.VITE_PERENUAL_API_KEY;
+
+const PerenualAPISearchEndpoint =
+  "https://perenual.com/plant-species-database-search-finder/species/";
+
+const edibleParts = ["any", "tubers/bulbs", "leaves/stems", "fruit/pods/seeds"];
+
+export { plantNamesList, API_KEY, edibleParts };
