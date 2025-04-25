44f745dd6da024fc02f45751a106514c372d169f
scmsal <31stephcm@gmail.com>
2025-04-25 11:00:30 -0400

    Update App.jsx to test useDispatch with test.txt

--- garden_app/src/App.jsx
+++ garden_app/src/App.jsx
@@ -2,12 +2,31 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import VeggiesList from "./components/VeggiesList";
-import { Row, Col } from "react-bootstrap";
 import QueryForm from "./components/QueryForm";
 import DisplayGroup from "./components/DisplayGroup";
 import PlantFetcher from "./components/PlantFetcher";
+import { Row, Col } from "react-bootstrap";
+import { useEffect } from "react";
+import { loadStarterPlants } from "./features/plantsSlice";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const testFetch = async () => {
+      try {
+        const response = await fetch("/test.txt");
+        const text = await response.text();
+        console.log("test file content:", text);
+      } catch (error) {
+        console.error("Error fetching test file:", error);
+      }
+    };
+    testFetch();
+    dispatch(loadStarterPlants());
+  }, [dispatch]);
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
