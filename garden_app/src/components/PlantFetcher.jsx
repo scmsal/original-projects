446f87daa3390284dd7d2c563f47da008f1939c5
scmsal <31stephcm@gmail.com>
2025-04-26 10:57:15 -0400

    added placeholder image for plant details card

--- garden_app/src/components/PlantFetcher.jsx
+++ garden_app/src/components/PlantFetcher.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Container, Card } from "react-bootstrap";
+import placeholderImg from "../assets/icons8-plant-80.png";
 
 function PlantFetcher() {
   const selectedPlant = useSelector((state) => state.plants.selectedPlant);
 
-  // console.log("Selected Plant: ", selectedPlant, selectedPlant.image);
+  console.log("Selected Plant: ", selectedPlant);
 
   if (!selectedPlant) {
     return <p>No plant selected</p>;
@@ -13,11 +14,37 @@ function PlantFetcher() {
 
   const { common_name, scientific_name, image } = selectedPlant;
 
-  console.log(image);
+  console.log("image: ", image);
+
   return (
     <Container className="mx-3 mb-3">
       <Card>
-        {image && <Card.Img variant="top" src={image} />}
+        <Card.Img
+          variant="top"
+          src={image || placeholderImg}
+          alt={image ? { common_name } : "Fallback plant image"}
+        />
+
+        {!image && (
+          <div className="text-center mt-2">
+            <small className="d-block">Image not available</small>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://icons8.com/icon/31725/plant"
+            >
+              Plant
+            </a>
+            <span>icon by </span>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://icons8.com"
+            >
+              Icons8
+            </a>
+          </div>
+        )}
         <Card.Body>
           <Card.Title className="text-center">{common_name}</Card.Title>
           <h6 className="text-center">{scientific_name}</h6>
