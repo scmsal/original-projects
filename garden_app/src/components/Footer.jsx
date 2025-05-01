5c73741ab162488948055c937e3e62413a06895e
scmsal <31stephcm@gmail.com>
2025-05-01 09:48:27 -0400

    footer content alignment

--- garden_app/src/components/Footer.jsx
+++ garden_app/src/components/Footer.jsx
@@ -4,13 +4,13 @@ import { Container, Row, Col } from "react-bootstrap";
 const Footer = () => {
   return (
     <footer className="bg-success bg-opacity-10 py-3 w-100 footer-bottom">
-      <Container fluid>
+      <Container fluid className="ms-3">
         <Row>
           <Col>Copyright &copy; 2025</Col>
           {/* <Col>
             <APICallCounter />
           </Col> */}
-          <Col className="text-end">
+          <Col className="text-center">
             Plant Data: {"  "}
             <a
               href="https://perenual.com"
@@ -31,13 +31,13 @@ const Footer = () => {
               RapidAPI
             </a>
           </Col>
-          <Col className="text-end">
+          <Col className="text-center">
             GIF via{"  "}
             <a href="https://giphy.com/gifs/tomato-tomates-potager-EGzDfFUauCo6nDgfpk">
               GIPHY
             </a>
           </Col>
-          <Col className="text-end">
+          <Col className="text-center">
             Favicon by{" "}
             <a href="https://www.freepik.com/free-vector/tomato-vegetable-organic-icon-isolated_169501930.htm#fromView=keyword&page=1&position=1&uuid=32966af5-595b-423f-b9b9-d08ce1490077&query=Tomato+Svg">
               Freepik
