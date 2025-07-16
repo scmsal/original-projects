881434b5716275dce82aaf67b3eb65089f483c89
scmsal <31stephcm@gmail.com>
2025-07-16 09:56:52 -0400

    added logo file;  experimented with project page layout and skills list rendering

--- SSal-portfolio/src/pages/projects.jsx
+++ SSal-portfolio/src/pages/projects.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import skillsData from "../data/skills.json";
 
 const ProjectsPage = () => {
-  return <h1>This is the projects page</h1>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-6">
+      <div className="grid md:col-span-2">
+        <h1 className="text-center mb-4">Tech Stack</h1>
+        <div className="">
+          {/* Tech Stack */}
+
+          {/* grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 */}
+          <div className="md:col-span-2 flex flex-wrap gap-6">
+            {skillsData.map((section) => {
+              return (
+                <div
+                  className="flex-1 min-w-24 text-left border border-b-dusty-blue p-4 rounded"
+                  key="section.category"
+                >
+                  <h2 className="font-bold text-3xl text-cobalt mb-4">
+                    {section.category}
+                  </h2>
+                  <div className="flex flex-col gap-4">
+                    {section.skills.map((skill) => {
+                      const name = skill.name;
+                      const logo = skill.logo;
+                      return (
+                        <div key={name} className="">
+                          {logo && <img className="h-8" src={skill.logo} />}
+                          <h3 className="font-bold">{name}</h3>
+
+                          <p>{skill.level}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      {/* Projects */}
+      <div className="md:col-span-1">
+        <h1 className="text-center">Projects</h1>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectsPage;
