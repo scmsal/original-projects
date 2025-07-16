9da18713fcd03c1cdf1c8c661925a95e019cb7ae
scmsal <31stephcm@gmail.com>
2025-07-16 09:55:31 -0400

    changed styling of footer

--- SSal-portfolio/src/components/footer.jsx
+++ SSal-portfolio/src/components/footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="text-sm text-cobalt bg-gray-100 mt-auto px-12 py-4">
+    <div className="text-sm text-cobalt mt-auto px-12 py-4">
       © {new Date().getFullYear()} Stephanie Salifu. All rights reserved.
     </div>
   );
