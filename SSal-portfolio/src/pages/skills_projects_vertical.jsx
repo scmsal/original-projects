2d64d053d32e16f82c2496b3b3da6f8affadfb9a
scmsal <31stephcm@gmail.com>
2025-07-24 14:21:21 -0400

    added state variables

--- SSal-portfolio/src/pages/skills_projects_vertical.jsx
+++ SSal-portfolio/src/pages/skills_projects_vertical.jsx
@@ -1,10 +1,10 @@
-import skillsData from "../data/skills.json";
+import skills from "../data/skills.json";
 import projects from "../data/projects.json";
 import trainings from "../data/trainings.json";
 import { NavLink } from "react-router-dom";
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
+import { useState, useEffect } from "react";
 
-// ${colorMap[skill.level]}
 const SkillsPage = () => {
   const levelMap = {
     1: "●○○○",
@@ -18,21 +18,47 @@ const SkillsPage = () => {
     3: "text-pink-500",
     4: "text-fuchsia-900",
   };
+  const [selectedSkill, setSelectedSkill] = useState("");
+  const [selectedTraining, setSelectedTraining] = useState("");
+  const [selectedProject, setSelectedProject] = useState("");
+
+  let isHighlighted;
+
+  const projectClickHandler = (project) => {
+    event.preventDefault();
+    console.log("projectClickHandler ran");
+    setSelectedProject(project === selectedProject ? null : project);
+    skills.map((category) => {
+      category.skills.map((skill) => {
+        isHighlighted = selectedProject?.skills.includes(skill.name);
+        console.log(skill.name, { isHighlighted });
+      });
+    });
+  };
+
+  const skillClickHandler = (skill) => {
+    setSelectedSkill(skill);
+  };
+
+  const trainingClickHandler = (training) => {
+    setSelectedTraining(training);
+  };
+
   return (
     <div className="grid grid-cols-3 gap-2 p-4">
       {/* Tech Stack */}
       <div className="col-span-2">
-        <h1 className="text-center mb-3">Tech Stack</h1>
+        <h1 className="text-center mb-3">Tech Stack Skills</h1>
         <div className="">
           {/* grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 */}
           <div className="flex flex-wrap justify-left gap-4">
-            {skillsData.map((section) => {
+            {skills.map((section) => {
               return (
                 <div
                   className="text-center w-2xs shadow-md p-2 rounded"
-                  key="section.category"
+                  key={section.category}
                 >
-                  <h2 className="font-bold text-2xl text-gray-600 mb-2">
+                  <h2 className="font-bold text-2xl text-gray-500">
                     {section.category}
                   </h2>
                   <ul className="">
@@ -45,13 +71,33 @@ const SkillsPage = () => {
                           className="flex items-center justify-between"
                         >
                           <div className="flex items-center ">
-                            <h3 className={`font-bold pr-3`}>{name}</h3>{" "}
+                            <h3
+                              className={`font-bold pr-3 cursor-pointer ${isHighlighted ? "bg-violet-700 text-white" : ""}`}
+                              onClick={() => {
+                                console.log("skill clicked", { skill });
+                                skillClickHandler(skill);
+                              }}
+                            >
+                              {name}
+                            </h3>
                             {/* {logo && <img className="h-6" src={skill.logo} />} */}
                           </div>
                           <div className="items-center">
-                            <span className={`text-cobalt `}>
-                              {levelMap[skill.level]}
-                            </span>
+                            {/* I was going to use a for loop to iteratively render the dots, but ChatGPT suggested using the following logic, which I adapted */}
+                            {[...Array(4)].map((__, i) => {
+                              return (
+                                <span
+                                  key={`dots${i}`}
+                                  className={
+                                    i + 1 <= skill.level
+                                      ? "text-cobalt"
+                                      : "text-gray-400"
+                                  }
+                                >
+                                  {i + 1 <= skill.level ? "●" : "○"}
+                                </span>
+                              );
+                            })}
                           </div>
                         </li>
                       );
@@ -64,53 +110,68 @@ const SkillsPage = () => {
         </div>
       </div>
 
-      <div className="col-span-1">
+      <div className="col-span-1 flex flex-col justify-around">
         <div className="mb-3">
           <h1 className="mb-3">...learned in:</h1>
           <div className="flex flex-wrap justify-center gap-3">
+            <span className="text-gray-600">
+              Programs and courses where I learned these skills.
+            </span>
+            <NavLink to="/about" className="">
+              <ArrowTopRightOnSquareIcon
+                className="text-violet-700  w-5 h-5"
+                title="View training"
+              />
+            </NavLink>
+
             {trainings.map((training) => {
               return (
                 <span
                   key={training.title}
-                  className="mb-1 border border-cobalt rounded-full px-2 cursor-pointer"
+                  className={`border ${isHighlighted ? "border-violet-500 bg-violet-700 text-white" : "border-cobalt"} rounded-full px-2 cursor-pointer mb-1`}
+                  title={training.details}
+                  onClick={() => {
+                    console.log("training clicked", training);
+                    trainingClickHandler(training);
+                  }}
                 >
-                  {training.title}{" "}
+                  {training.title}
                 </span>
               );
             })}
-            <div className="flex items-center justify-end">
-              <NavLink to="/about" className="flex  gap-2">
+            <div className="flex items-center justify-end"></div>
+          </div>
+        </div>
+        <div className="col-span-1 flex flex-col justify-around">
+          <div className="mb-3">
+            <h1 className="mb-3">...applied in:</h1>
+            <div className="flex flex-wrap justify-center gap-3">
+              <span className="text-gray-600">
+                Projects in which I used these skills.
+              </span>
+              <NavLink to="/about" className="">
                 <ArrowTopRightOnSquareIcon
-                  className="text-fuchsia-500 w-5 h-5"
-                  title="View training"
+                  className="text-violet-700  w-5 h-5"
+                  title="View projects"
                 />
               </NavLink>
             </div>
-          </div>
-        </div>
-        <div>
-          <div className="flex">
-            <h1 className="mb-3">...applied in:</h1>
           </div>
           <div className="flex flex-wrap justify-center gap-3">
             {projects.map((project) => {
               return (
                 <span
                   key={project.name}
-                  className="border border-cobalt rounded-full px-2 cursor-pointer mb-1"
+                  className={`border ${isHighlighted ? "border-violet-500 bg-violet-700 text-white" : "border-cobalt"} rounded-full px-2 cursor-pointer mb-1`}
+                  onClick={() => {
+                    console.log("project clicked", project);
+                    projectClickHandler(project);
+                  }}
                 >
                   {project.name}
                 </span>
               );
             })}
-            <div className="flex items-center justify-end">
-              <NavLink to="/about" className="flex  gap-2">
-                <ArrowTopRightOnSquareIcon
-                  className="text-fuchsia-500 w-5 h-5"
-                  title="View projects"
-                />
-              </NavLink>
-            </div>
           </div>
         </div>
       </div>
