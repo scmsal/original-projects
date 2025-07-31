37f4604a205ef97efa38100100bdac326c1c4062
scmsal <31stephcm@gmail.com>
2025-07-31 15:11:18 -0400

    filled contact page

--- SSal-portfolio/src/pages/contact.jsx
+++ SSal-portfolio/src/pages/contact.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const ContactPage = () => {
-  return <h1>This is the Contact page</h1>;
+  return (
+    <main className="flex-grow flex pt-6 justify-center">
+      <div className="flex flex-col justify-center px-20 mt-30 w-2xl h-60 pt-4 shadow">
+        <h1 className="mb-4">Contact me</h1>
+        <p>
+          If you have any questions or would like to connect, you can reach me
+          at{" "}
+          <a
+            href="mailto:scm.salifu@gmail.com"
+            className="text-fuchsia-700 hover:underline font-bold"
+          >
+            scm.salifu@gmail.com
+          </a>{" "}
+          or on{" "}
+          <a
+            href="https://www.linkedin.com/in/stephanie-s-a7091854/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-fuchsia-700 hover:underline"
+          >
+            {" "}
+            LinkedIn
+          </a>
+          .
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default ContactPage;
