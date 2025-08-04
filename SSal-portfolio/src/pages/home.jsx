700239fb61a0cf626fe2ee69400100657cb1b9b7
scmsal <31stephcm@gmail.com>
2025-08-04 13:59:18 -0400

    added image

--- SSal-portfolio/src/pages/home.jsx
+++ SSal-portfolio/src/pages/home.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import NavBar from "../components/NavBar";
 
 const HomePage = () => {
-  console.log("HomePage rendered");
-  const lang = ["EN", "FR", "ES", "IT"];
-  const complang = ["JS", "TS", "CSS", "HTML"];
   return (
     // <div className="bg-[url(./assets/boliviainteligente-8H8Y5emrdO0-unsplash-orig.jpg)] bg-cover bg-center">
     // <div className="bg-linear-to-r/hsl from-indigo-900 from-20%  via-purple-500 via-75% to-pink-500 to-90%">
 
     <main className="flex flex-grow flex-col justify-center items-start">
       <div className="flex px-8 pt-8 ">
-        <div className="">
+        <div className="flex flex-col items-start justify-center w-2/3">
           <div>
             <span className="font-bold uppercase text-6xl font-upper text-transparent bg-clip-text bg-linear-to-r from-cobalt from-10%  via-purple-500 via-75% to-pink-500 to-90%">
               Frontend developer
@@ -27,6 +24,13 @@ const HomePage = () => {
               internationalization.
             </p>
           </div>
+        </div>
+        <div className="flex flex-col items-center justify-center w-1/3">
+          <img
+            src="/project_imgs/wordcloud-2.png"
+            alt="developer word cloud"
+            className="object-cover w-full h-full"
+          />
         </div>
       </div>
     </main>
